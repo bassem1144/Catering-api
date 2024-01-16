78cfeb3ef78acbb280a389e465b3f58047e351b1
Bassem <bassem.ismail@student.rocva.nl>
2024-01-16 20:58:02 +0100

    added routes for facility controller

--- routes/routes.php
+++ routes/routes.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Controllers\IndexController;
+
 /** @var Bramus\Router\Router $router */
 
 // Define routes here
-$router->get('/test', App\Controllers\IndexController::class . '@test');
-$router->get('/', App\Controllers\IndexController::class . '@test');
+$router->get('/test', IndexController::class . '@test');
+$router->get('/', IndexController::class . '@test');
+
+// Define routes for the facility controller
+$router->post('/api/facilities', [FacilityController::class, 'create']); // Create a facility and its tags
+$router->get('/api/facilities/{id}', [FacilityController::class, 'read']); // Read one facility, its location, and its tags
+$router->get('/api/facilities', [FacilityController::class, 'readAll']); // Read multiple facilities, their location, and their tags
+$router->put('/api/facilities/{id}', [FacilityController::class, 'update']); // Update a facility and its tags
+$router->delete('/api/facilities/{id}', [FacilityController::class, 'delete']); // Delete a facility and its tags
