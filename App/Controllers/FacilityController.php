25821a2e6a47d431095a58618f1140f8abee033c
Bassem <bassem.ismail@student.rocva.nl>
2024-01-18 11:53:46 +0100

    added try and catch error handling

--- App/Controllers/FacilityController.php
+++ App/Controllers/FacilityController.php
@@ -11,34 +11,50 @@ class FacilityController extends BaseController
 
     public function create()
     {
-        $facility = new Facility;
-        $facility->setName($_POST['name']);
-        $facility->setCreationDate();
+        try {
+            $facility = new Facility;
+            $facility->setName($_POST['name']);
+            $facility->setCreationDate();
 
-        $location = new Location;
-        $location->setCity($_POST['city']);
-        $location->setAddress($_POST['address']);
-        $location->setZipCode($_POST['zip_code']);
-        $location->setCountryCode($_POST['country_code']);
-        $location->setPhoneNumber($_POST['phone_number']);
+            $location = new Location;
+            $location->setCity($_POST['city']);
+            $location->setAddress($_POST['address']);
+            $location->setZipCode($_POST['zip_code']);
+            $location->setCountryCode($_POST['country_code']);
+            $location->setPhoneNumber($_POST['phone_number']);
 
-        // Associate the location with the facility
-        $facility->setLocation($location);
+            // Associate the location with the facility
+            $facility->setLocation($location);
 
-        $this->db->beginTransaction();
+            // Begin a transaction
+            $this->db->beginTransaction();
 
-        $locationQuery = "INSERT INTO locations (city, address, zip_code, country_code, phone_number) VALUES (?, ?, ?, ?, ?)";
-        $locationBind = [$location->getCity(), $location->getAddress(), $location->getZipCode(), $location->getCountryCode(), $location->getPhoneNumber()];
-        $this->db->executeQuery($locationQuery, $locationBind);
+            // Insert the location into the database
+            $locationQuery = "INSERT INTO locations (city, address, zip_code, country_code, phone_number) VALUES (?, ?, ?, ?, ?)";
+            $locationBind = [$location->getCity(), $location->getAddress(), $location->getZipCode(), $location->getCountryCode(), $location->getPhoneNumber()];
+            $this->db->executeQuery($locationQuery, $locationBind);
 
-        // Get the location ID from the last inserted row
-        $locationId = $this->db->getLastInsertedId();
+            // Get the location ID from the last inserted row
+            $locationId = $this->db->getLastInsertedId();
 
-        // Insert the facility into the database with the associated location ID
-        $facilityQuery = "INSERT INTO facilities (name, creation_date, location_id) VALUES (?, ?, ?)";
-        $facilityBind = [$facility->getName(), $facility->getCreationDate(), $locationId];
-        $this->db->executeQuery($facilityQuery, $facilityBind);
+            // Insert the facility into the database with the associated location ID
+            $facilityQuery = "INSERT INTO facilities (name, creation_date, location_id) VALUES (?, ?, ?)";
+            $facilityBind = [$facility->getName(), $facility->getCreationDate(), $locationId];
+            $this->db->executeQuery($facilityQuery, $facilityBind);
 
-        $this->db->commit();
+            // Commit the transaction
+            $this->db->commit();
+            echo "Facility and Location created successfully!";
+        } catch (PDOException $e) {
+            // Rollback the transaction in case of a database error
+            $this->db->rollBack();
+
+            // Handle database errors 
+            echo "Database Error: " . $e->getMessage();
+        } catch (Exception $e) {
+            // Handle other errors
+            echo "Error: " . $e->getMessage();
+        }
     }
+
 }
